6f694242ef9037fcb6c12621731161b6743b1f8c
Ihor Komarov <ihor.komarov@studium.uni-hamburg.de>
2021-05-28 23:12:45 +0200

    removed phi plot due to undiscovered bug

--- include/HHonlineSelectionHists.h
+++ include/HHonlineSelectionHists.h
@@ -19,11 +19,11 @@ public:
     virtual void fill(const uhh2::Event & ev) override;
     virtual ~HHonlineSelectionHists();
 protected:
-    TH1F *n_L1mu,*pt_L1mu, *eta_L1mu, *phi_L1mu;
-    TH1F *n_Mu,*pt_Mu, *eta_Mu, *phi_Mu;  
+    // TH1F *n_L1mu, *pt_L1mu, *eta_L1mu, *phi_L1mu;
+    // TH1F *n_Mu,*pt_Mu, *eta_Mu, *phi_Mu;  
 
-    TH1F *eta_L1jet1, *pt_L1jet1, *phi_L1jet1, *eta_L1jet2, *eta_L1jet3, *eta_L1jet4, *n_L1Jets;
-    TH1F *eta_jet1, *pt_jet1,*phi_jet1, *eta_jet2, *eta_jet3, *eta_jet4, *n_Jets;
+    // TH1F *eta_L1jet1, *pt_L1jet1, *phi_L1jet1, *eta_L1jet2, *eta_L1jet3, *eta_L1jet4, *n_L1Jets;
+    // TH1F *eta_jet1, *pt_jet1, *phi_jet1, *eta_jet2, *eta_jet3, *eta_jet4, *n_Jets;
 
     TH1F *leading_L1Mu_pt, *leading_Mu_pt;
     TH1F *relative_error_Muon, *relative_error_Jet;
